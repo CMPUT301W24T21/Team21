adaedbe6152b830645de5485265a5a7f2d2543ca
AidenKly <112991248+AidenKly@users.noreply.github.com>
2024-02-01 15:00:26 -0700

    removed shape color again so that paul can do things in correct order

--- app/src/main/java/com/example/team21/Shape.java
+++ app/src/main/java/com/example/team21/Shape.java
@@ -3,7 +3,6 @@ package com.example.team21;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    String color = "Magenta"; //This is the main color of shape
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
