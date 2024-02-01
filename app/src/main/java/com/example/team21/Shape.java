a089d8ef597c4577d0c8ac241decf0fb98637910
AidenKly <aklymchu@ualberta.ca>
2024-02-01 11:09:08 -0700

    added Shape class

---
+++ app/src/main/java/com/example/team21/Shape.java
@@ -0,0 +1,10 @@
+package com.example.team21;
+
+public abstract class Shape {
+    protected Integer x;
+    protected Integer y;
+    public Shape(Integer x, Integer y) {
+        this.x = x;
+        this.y = y;
+    }
+}
