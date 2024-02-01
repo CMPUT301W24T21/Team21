b45696db9b682bad756e317adf9efd6523f868da
Kirill Kotlyarchuk <kotlyarc@ualberta.ca>
2024-02-01 15:09:53 -0700

    // added a colour string field

--- app/src/main/java/com/example/team21/Shape.java
+++ app/src/main/java/com/example/team21/Shape.java
@@ -3,6 +3,7 @@ package com.example.team21;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
+    String color = "Indigo";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
