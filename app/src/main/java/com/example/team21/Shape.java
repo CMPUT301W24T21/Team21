b73cd0649e1f0ab80c1955edb32e849ebea99578
Dhaval Patel <91800444+Homer20@users.noreply.github.com>
2024-02-01 18:02:37 -0700

    Update Shape.java

--- app/src/main/java/com/example/team21/Shape.java
+++ app/src/main/java/com/example/team21/Shape.java
@@ -3,6 +3,7 @@ package com.example.team21;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
+    protected String color = "blue";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
