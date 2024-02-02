bc23559818c3401b50c48049e7a7e53ecb7f565e
Dhaval Patel <dhaval@ualberta.ca>
2024-02-02 11:20:22 -0700

    Updated shape on local branch

--- app/src/main/java/com/example/team21/Shape.java
+++ app/src/main/java/com/example/team21/Shape.java
@@ -3,7 +3,7 @@ package com.example.team21;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    protected String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    protected String color = "blue";
     public Shape(Integer x, Integer y) {
         this.x = x;
         this.y = y;
